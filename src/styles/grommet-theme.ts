cf4d69a8a8216f485c8e4a473494f43990dc3028
Taras Tsupryk <taras.tsupryk@intellias.com>
2023-11-20 01:34:53 +0200

    Fix theme

--- src/styles/grommet-theme.ts
+++ src/styles/grommet-theme.ts
@@ -1,8 +1,14 @@
+import { ThemeType } from "grommet";
 import { theme } from "./theme";
 
-export const grommetTheme = {
+export const grommetTheme: ThemeType = {
   global: {
     colors: theme.colors,
     size: theme.spacings,
+    edgeSize: {
+      none: "0px",
+      ...theme.spacings,
+      ...theme.rounds,
+    },
   },
 };
