9da6904f6f933cce0b9339641646c16369e2c02a
Taras Tsupryk <taras.tsupryk@intellias.com>
2023-12-17 19:04:52 +0200

    Fix breadcrumgs

--- src/components/icons/ChevronIcon.tsx
+++ src/components/icons/ChevronIcon.tsx
@@ -3,19 +3,18 @@ import { Blank, Icon } from "grommet-icons";
 
 export const ChevronIcon: Icon = (props) => (
   <Blank
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="8"
-    viewBox="0 0 14 8"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
     fill="none"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
-      d="M1 1L7 7L13 1"
-      stroke="#56606D"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M5.33301 4.27631L6.27587 3.3335L10.9899 8.04755L6.27587 12.7616L5.33301 11.8187L9.10427 8.04755L5.33301 4.27631Z"
+      fill="none"
     />
   </Blank>
 );
--- src/pages/Task.Page.tsx
+++ src/pages/Task.Page.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { PageWrapper } from "components/wrappers/PageWrapper";
 import { Task } from "modules/Task";
+import { Box, Text } from "grommet";
+import { ChevronIcon } from "components/icons";
 
 const TaskPage = () => {
   return (
     <PageWrapper>
-      <Task />
+      <Box direction="row" align="center">
+        <Text color="textSecondary" weight={700}>Курси</Text>
+        <Box margin={{ horizontal: 'spacing8' }}><ChevronIcon size="spacing16" /></Box>
+        <Text color="textSecondary" weight={700}>Python. Основи програмування мовою.</Text>
+        <Box margin={{ horizontal: 'spacing8' }}><ChevronIcon size="spacing16" /></Box>
+        <Text color="textSecondary" weight={700}>Лекція 4. Функції з Python</Text>
+        <Box margin={{ horizontal: 'spacing8' }}><ChevronIcon size="spacing16" /></Box>
+        <Text color="textPrimary" weight={700}>Задача 13. Порахуйте кількість кубиків із фарбою</Text>
+      </Box>
+      <Box margin={{ top: "spacing28" }}>
+        <Task />
+      </Box>
     </PageWrapper>
   );
 };
