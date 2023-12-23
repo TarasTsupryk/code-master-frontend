0e62bd2e1b03711590746a4e5ea340ddb3b59efd
Taras Tsupryk <taras.tsupryk@intellias.com>
2023-12-24 01:52:36 +0200

    Clean up

--- src/modules/Task/Task.tsx
+++ src/modules/Task/Task.tsx
@@ -1,25 +1,10 @@
 import React from "react";
 import { Box } from "grommet";
-import { Editor } from "components/controls/Editor";
-import { CheckList } from "components/elements/CheckList";
-import useTaskCheckList from "./hooks/useTaskCheckList";
 
 const Task = () => {
-  const taskCheckList = useTaskCheckList();
   return (
     <Box direction="row">
-      <Box width="100%">
-        <Editor />
-      </Box>
-      <Box
-        margin={{ left: "30px" }}
-        round="spacing12"
-        background="white"
-        pad="spacing32"
-        width={{ width: "100%", max: "320px" }}
-      >
-        <CheckList title="Умови виконання задачі" items={taskCheckList} />
-      </Box>
+      Task3123
     </Box>
   );
 };
