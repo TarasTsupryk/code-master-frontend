0d649d67c5c3e8aa5ed3b6aa87f5e6bd5bcb25af
Taras Tsupryk <taras.tsupryk@intellias.com>
2023-12-24 02:04:41 +0200

    Add animation

--- src/components/wrappers/PageWrapper/PageWrapper.tsx
+++ src/components/wrappers/PageWrapper/PageWrapper.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { Box } from "grommet";
+import styled from "styled-components";
+
+const PageWithAnimation = styled(Box)`
+  animation: 0.5s hideIn;
+
+  @keyframes hideIn {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+`;
 
 interface Props {
   children?: React.ReactNode;
 }
 
 const PageWrapper: React.FC<Props> = ({ children }) => (
-  <Box pad="spacing28">{children}</Box>
+  <PageWithAnimation pad="spacing28">{children}</PageWithAnimation>
 );
 
 export default PageWrapper;
